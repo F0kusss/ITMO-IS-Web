ad4bee4c3e0a35105b1f1d955daa010619ce3192
F0kusss <kulikovavita9@gmail.com>
2022-12-24 21:35:50 +0300

    feat: added exchanges page

--- scripts/cryptocurrencies-logic.js
+++ scripts/cryptocurrencies-logic.js
@@ -1,24 +1,31 @@
 var baseURL = "https://api.coingecko.com/api/v3";
 
-const ITEMS_PER_PAGE = 30;
+const ITEMS_PER_PAGE = 100;
 const PAGES_NUMBER = 5;
 
 function load_table_page(page_number) {
-  const table_element = document.getElementById('tableBody1');
+  const table_element = document.getElementById('cryptocurrencies_table');
   table_element.innerHTML = "";
   var coinDataURL = "/coins/markets?vs_currency=usd&order=market_cap_desc%2C%20volume_desc%2C%20id_desc&per_page=" + ITEMS_PER_PAGE + "&page=" + page_number + "&sparkline=false&price_change_percentage=24h";
   fetch(baseURL + coinDataURL)
   .then(res =>{
     res.json().then(data =>{
       for(var i = 0; i < ITEMS_PER_PAGE; i++){
-        let rank = data[i].market_cap_rank;
+        let rank = ITEMS_PER_PAGE * (page_number - 1) + i + 1;
         let image = data[i].image;
         let coin = data[i].name;
         let mcap = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(data[i].market_cap);
         let price = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(data[i].current_price);
         let volume = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(data[i].total_volume);
         let supply = new Intl.NumberFormat("en-GB").format(data[i].circulating_supply.toFixed(0));
-        let change = data[i].price_change_percentage_24h.toFixed(2);
+        let change;
+        try {
+          change = data[i].price_change_percentage_24h.toFixed(2);
+        }
+        catch(e) {
+          change = 0;
+        }
+
         let positive = "+";
 
         let tableRow = "<tr><td class='rank__column'>"+ rank + "</td>" + "<td><img class= 'image__column' src= " + image + "/>" + "<div class='coin__column'>" + 
@@ -26,17 +33,17 @@ function load_table_page(page_number) {
         "<td class='volume__column'>" + volume + "</td>"+ "<td class='supply__column'>" + supply + "</td>";
       
         if(change < 0){
-          $("#tableBody1").append(tableRow + "<td class='red__column'>"+ change + "%</td></tr>");
+          $("#cryptocurrencies_table").append(tableRow + "<td class='red__column'>"+ change + "%</td></tr>");
         }
         
         else {
-          $("#tableBody1").append(tableRow + "<td class='green__column'>"+ positive + change + "%</td></tr>");
+          $("#cryptocurrencies_table").append(tableRow + "<td class='green__column'>"+ positive + change + "%</td></tr>");
         };
       }
     })
   })
   .catch(err =>{
-    $("#tableBody1").html(err);
+    $("#cryptocurrencies_table").html(err);
   });
 }
 
